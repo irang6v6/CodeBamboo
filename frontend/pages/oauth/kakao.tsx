b429db8f9379f601bcb3b320a48ed632d3385be3
chanshin0 <windcj1@naver.com>
2023-04-26 18:03:38 +0900

    kakao 로그인 토큰 받아오기

--- frontend/pages/oauth/kakao.tsx
+++ frontend/pages/oauth/kakao.tsx
@@ -1,28 +1,33 @@
 import { Inter } from 'next/font/google'
-import { useQuery } from 'react-query'
+import { useMutation, useQuery } from 'react-query'
 
 const inter = Inter({ subsets: ['latin'] })
 
-const fetchUser = async () => {
+const fetchUser = async (code:string) => {
   const BASE_URL = 'http://localhost:8000'
-  const response = await fetch(BASE_URL+'/users/1', {
-    method: 'GET',
+  const response = await fetch(BASE_URL+'/users', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ code }),
   })
   const data = await response.json()
   return data
 }
 
 export default function Kakao() {
+  // let code = new URL(window.location.href).searchParams.get("code");
+  // const { isLoading, isError, data: seoyong } = useMutation('user', ()=>fetchUser(code))
+  // console.log(code)
 
-  const { isLoading, isError, data: seoyong } = useQuery('user', fetchUser)
+  // if (isLoading) {
+  //   return <div>Loading...</div>
+  // }
 
-  if (isLoading) {
-    return <div>Loading...</div>
-  }
-
-  if (isError) {
-    return <div>Error fetching user data</div>
-  }
+  // if (isError) {
+  //   return <div>Error fetching user data</div>
+  // }
 
   return (
     <>
@@ -30,3 +35,5 @@ export default function Kakao() {
     </>
   );
 }
+
+// yOXSN4F7twnquMh3zj_ZvjXjJo4NXtLBS5sdYYl3qNDnyDhS1W7AqpDxU3Bo6JHu-3vB8gopyNkAAAGHu4BXng
